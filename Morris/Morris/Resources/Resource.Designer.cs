db5df42c8de2a460b5650065e882896eaa5ac2a2
usbcable <patricka1234@gmail.com>
2016-07-04 18:43:55 +0200

    swag

--- Morris/Morris/Resources/Resource.Designer.cs
+++ Morris/Morris/Resources/Resource.Designer.cs
@@ -1628,8 +1628,8 @@ namespace Morris
 			// aapt resource value: 0x7f0b0019
 			public const int beginning = 2131427353;
 			
-			// aapt resource value: 0x7f0b0089
-			public const int bindestreck = 2131427465;
+			// aapt resource value: 0x7f0b0088
+			public const int bindestreck = 2131427464;
 			
 			// aapt resource value: 0x7f0b0093
 			public const int bindestreck1 = 2131427475;
@@ -1664,8 +1664,8 @@ namespace Morris
 			// aapt resource value: 0x7f0b0097
 			public const int btnremovefriend = 2131427479;
 			
-			// aapt resource value: 0x7f0b0084
-			public const int button1 = 2131427460;
+			// aapt resource value: 0x7f0b0083
+			public const int button1 = 2131427459;
 			
 			// aapt resource value: 0x7f0b008f
 			public const int buttonAccept1 = 2131427471;
@@ -1673,8 +1673,8 @@ namespace Morris
 			// aapt resource value: 0x7f0b0090
 			public const int buttonDecline1 = 2131427472;
 			
-			// aapt resource value: 0x7f0b0086
-			public const int buttonInviteFriend = 2131427462;
+			// aapt resource value: 0x7f0b0085
+			public const int buttonInviteFriend = 2131427461;
 			
 			// aapt resource value: 0x7f0b002b
 			public const int buttonPanel = 2131427371;
@@ -1844,14 +1844,14 @@ namespace Morris
 			// aapt resource value: 0x7f0b006b
 			public const int relativeLayout1 = 2131427435;
 			
-			// aapt resource value: 0x7f0b008b
-			public const int rowdate = 2131427467;
+			// aapt resource value: 0x7f0b008a
+			public const int rowdate = 2131427466;
 			
 			// aapt resource value: 0x7f0b0095
 			public const int rowdate1 = 2131427477;
 			
-			// aapt resource value: 0x7f0b0085
-			public const int roweventdescription = 2131427461;
+			// aapt resource value: 0x7f0b0084
+			public const int roweventdescription = 2131427460;
 			
 			// aapt resource value: 0x7f0b008e
 			public const int roweventinvitedescription = 2131427470;
@@ -1952,8 +1952,8 @@ namespace Morris
 			// aapt resource value: 0x7f0b007c
 			public const int text2 = 2131427452;
 			
-			// aapt resource value: 0x7f0b0087
-			public const int textLocation = 2131427463;
+			// aapt resource value: 0x7f0b0086
+			public const int textLocation = 2131427462;
 			
 			// aapt resource value: 0x7f0b0034
 			public const int textSpacerNoButtons = 2131427380;
@@ -1970,20 +1970,20 @@ namespace Morris
 			// aapt resource value: 0x7f0b0055
 			public const int theLocation = 2131427413;
 			
-			// aapt resource value: 0x7f0b0083
-			public const int theweek = 2131427459;
+			// aapt resource value: 0x7f0b008b
+			public const int theweek = 2131427467;
 			
 			// aapt resource value: 0x7f0b007a
 			public const int time = 2131427450;
 			
-			// aapt resource value: 0x7f0b0088
-			public const int timefrom = 2131427464;
+			// aapt resource value: 0x7f0b0087
+			public const int timefrom = 2131427463;
 			
 			// aapt resource value: 0x7f0b0092
 			public const int timefrom1 = 2131427474;
 			
-			// aapt resource value: 0x7f0b008a
-			public const int timeto = 2131427466;
+			// aapt resource value: 0x7f0b0089
+			public const int timeto = 2131427465;
 			
 			// aapt resource value: 0x7f0b0094
 			public const int timeto1 = 2131427476;
